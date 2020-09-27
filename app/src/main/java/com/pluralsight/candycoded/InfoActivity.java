0c4b277632b4ace7c44192d751633479405ca019
Bridget <bridgetshidok@gmail.com>
2020-09-27 14:09:08 +0100

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,11 @@
 package com.pluralsight.candycoded;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -20,6 +23,18 @@ public class InfoActivity extends AppCompatActivity {
                 load(uri).
                 into(candyStoreImageView);
 
+
+    }
+
+    public void createMapIntent(View view){
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if(getIntent() != null){
+            mapIntent.resolveActivity(getPackageManager());
+            startActivity(mapIntent);
+        }
 
     }
 
